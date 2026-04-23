6f99e5d14bb0e9aa77b3e2c1e2f77ab57f0854ed
stereotic <agorsunov70@gmail.com>
2026-04-23 20:54:32 +0400

    Consolidate API into single serverless function for Vercel Hobby plan

--- api/auth/send-code.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ success: false, msg: 'Method not allowed' });
-  }
-
-  res.json({ success: true, msg: 'Verification code sent (demo)' });
-};
